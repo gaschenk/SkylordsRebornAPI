c85ec5eb053f01dba3c269a658c6952caca38129
RaiNote <Timothy.Lutz@gmx.de>
2021-01-15 13:28:25 +0100

    sample1 added basic request based on new approach

---
+++ Sample1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SkylordsRebornAPI.Cardbase;
+
+namespace Sample1
+{
+    class Program
+    {
+        private static readonly JsonSerializerSettings Settings = new() {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new StringEnumConverter()
+            }
+        };
+
+        static void Main() {
+            var x = CardService.HandleCardRequest(new List<Tuple<CardRequestProperty, string>>
+            {
+                new(CardRequestProperty.Name,"Gro"),
+                new(CardRequestProperty.Defense,"1100")
+            });
+            foreach(var card in x) {
+                Console.WriteLine(JsonConvert.SerializeObject(card, Settings));
+            }
+        }
+    }
+}
