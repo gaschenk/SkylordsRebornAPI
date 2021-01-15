94613233e716c288ffbb2882cf9bdcfcfc2680b7
RaiNote <Timothy.Lutz@gmx.de>
2021-01-15 13:42:18 +0100

    old request functions removed

--- SkylordsRebornAPI/Cardbase/CardService.cs
+++ SkylordsRebornAPI/Cardbase/CardService.cs
@@ -7,81 +7,18 @@ using SkylordsRebornAPI.Cardbase.Cards;
 
 namespace SkylordsRebornAPI.Cardbase
 {
-    public static class CardService
+    public class CardService
     {
-        private static string baseUrl = "https://cardbase.skylords.eu/";
+        private readonly string baseUrl = "https://cardbase.skylords.eu/";
 
-        public static Card[] GetCardsByName(string name)
-        {
-            var webRequest = WebRequestCreator($"Cards/GetCards?Name={name}");
-            var response = webRequest.GetResponse();
-            string text;
-            using (var sr = new StreamReader(response.GetResponseStream()!))
-            {
-                text = sr.ReadToEnd();
-            }
-
-            var cards = JsonConvert.DeserializeObject<APIWrap<Card>>(text).Result;
-            return cards;
-        }
-
-        public static Card[] GetCardsByCardType(CardType type)
-        {
-            var webRequest = WebRequestCreator($"Cards/GetCards?CardType={(int) type}");
-            var response = webRequest.GetResponse();
-            string text;
-            using (var sr = new StreamReader(response.GetResponseStream()!))
-            {
-                text = sr.ReadToEnd();
-            }
-
-            var cards = JsonConvert.DeserializeObject<APIWrap<Card>>(text).Result;
-            return cards;
-        }
-
-        public static Card[] GetCardsByCategory(string category)
-        {
-            var webRequest = WebRequestCreator($"Cards/GetCards?Category={category}");
-            var response = webRequest.GetResponse();
-            string text;
-            using (var sr = new StreamReader(response.GetResponseStream()!))
-            {
-                text = sr.ReadToEnd();
-            }
-
-            var cards = JsonConvert.DeserializeObject<APIWrap<Card>>(text).Result;
-            return cards;
-        }
-
-        public static Card[] GetCardsByCost(int cost)
-        {
-            var webRequest = WebRequestCreator($"Cards/GetCards?Cost={cost}");
-            var response = webRequest.GetResponse();
-            string text;
-            using (var sr = new StreamReader(response.GetResponseStream()!))
-            {
-                text = sr.ReadToEnd();
-            }
-
-            var cards = JsonConvert.DeserializeObject<APIWrap<Card>>(text).Result;
-            return cards;
-        }
-
-        private static WebRequest WebRequestCreator(string additionalPath)
-        {
-            var webRequest = WebRequest.Create($"{baseUrl}{additionalPath}");
-            webRequest.ContentType = "application/json; charset=utf-8";
-            return webRequest;
-        }
-
-        public static Card[] HandleCardRequest(List<Tuple<CardRequestProperty, String>> requestProperties)
+        public Card[] HandleCardRequest(List<Tuple<RequestProperty, String>> requestProperties)
         {
             string url = $"{baseUrl}Cards/GetCards?";
             for (var index = 0; index < requestProperties.Count; index++)
             {
                 var requestProperty = requestProperties[index];
                 url +=
-                    $"{(index > 0 ? "&" : "")}{Enum.GetName(typeof(CardRequestProperty), requestProperty.Item1)}={requestProperty.Item2}";
+                    $"{(index > 0 ? "&" : "")}{Enum.GetName(typeof(RequestProperty), requestProperty.Item1)}={requestProperty.Item2}";
             }
 
             var webRequest = WebRequest.Create(url);
@@ -96,14 +33,5 @@ namespace SkylordsRebornAPI.Cardbase
             var cards = JsonConvert.DeserializeObject<APIWrap<Card>>(text).Result;
             return cards;
         }
-    }
-
-    public enum CardRequestProperty
-    {
-        Name,
-        Cost,
-        CardType,
-        Category,
-        Defense,
     }
 }
