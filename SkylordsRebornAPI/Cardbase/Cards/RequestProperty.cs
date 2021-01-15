8be31458f0290a7d93dda3275c18e5e6e6158b7a
RaiNote <Timothy.Lutz@gmx.de>
2021-01-15 13:42:24 +0100

    old request functions removed

---
+++ SkylordsRebornAPI/Cardbase/Cards/RequestProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace SkylordsRebornAPI.Cardbase.Cards
+{
+    public enum RequestProperty
+    {
+        Name,
+        Cost,
+        CardType,
+        Category,
+        Defense,
+    }
+}
