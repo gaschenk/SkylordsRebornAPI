cc141f5637b5e04e7e4e610b797824f8b87bf905
RaiNote <Timothy.Lutz@gmx.de>
2021-01-16 19:08:19 +0100

    more request properties added & marked the ones which aren't working.

--- SkylordsRebornAPI/Cardbase/Cards/RequestProperty.cs
+++ SkylordsRebornAPI/Cardbase/Cards/RequestProperty.cs
@@ -4,8 +4,34 @@
     {
         Name,
         Cost,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
         CardType,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
+        Color,
         Category,
         Defense,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
+        Edition,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
+        Rarity,
+        Offense,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
+        DefenseType,
+        /// <summary>
+        /// Doesn't work
+        /// </summary>
+        OffenseType,
+        UnitCount,
+        ChargeCount,
     }
 }
