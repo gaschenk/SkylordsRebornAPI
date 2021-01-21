394a65c4da08cb3a075383c485c597615d09c3ce
RaiNote <Timothy.Lutz@gmx.de>
2021-01-21 12:26:56 +0100

    Renaming of stuff

---
+++ SkylordsRebornAPI.Replay/GodClassReplayKeyHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using SkylordsRebornAPI.Replay.ReplayKeys;
+
+namespace SkylordsRebornAPI.Replay
+{
+    public class GodClassReplayKeyHandler
+    {
+        internal AbstractReplayKey Handle(Data.ReplayKeys replayKey, BinaryReader reader)
+        {
+            return replayKey switch
+            {
+                Data.ReplayKeys.Attack => new Attack(reader),
+                _ => throw new Exception($"Unknown Replay Key {(int) replayKey}")
+            };
+        }
+    }
+}
