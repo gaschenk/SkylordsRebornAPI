664f34866414b9a43224a897ff32715a69b0dccf
RaiNote <Timothy.Lutz@gmx.de>
2021-01-21 12:41:07 +0100

    Update More Keys

--- SkylordsRebornAPI.Replay/Data/ReplayKeys.cs
+++ SkylordsRebornAPI.Replay/Data/ReplayKeys.cs
@@ -2,16 +2,27 @@
 {
     public enum ReplayKeys
     {
-        Move = 4013,
-        Attack = 4015,
+        LeaveGame = 4002,
+        PveUnknownA6 = 4006,
+        PveUnknownA8 = 4008,
         Summon = 4009,
         Spell = 4010,
         LineSpell = 4011,
         Build = 4012,
-        LeaveGame = 4002,
-        Destruct = 4041,
-        CreateOrb = 4031,
+        Move = 4013,
+        UseUnitAbility = 4014,
+        Attack = 4015,
+        StopUnit = 4019,
+        HoldPositionUnit = 4020,
+        UnknownBb= 4027,
+        ToggleWallGate =4028,
+        BuildWall = 4029,
         CreateMana = 4030,
-        UseUnitAbility = 4014
+        CreateOrb = 4031,
+        UnknownC1 = 4033,
+        UnknownC2 = 4034,
+        AbilityC3 = 4035,
+        DestructC9 = 4041,
+        PveUnknownCb =4043
     }
 }
