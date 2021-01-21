8a43bcc3f0815ab873e50b0339112072262548ba
RaiNote <Timothy.Lutz@gmx.de>
2021-01-21 18:18:31 +0100

    More ReplayKey Decoder

---
+++ SkylordsRebornAPI.Replay/ReplayKeys/LinearSpell.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace SkylordsRebornAPI.Replay.ReplayKeys
+{
+    [KeyDecoder(Data.ReplayKeys.LineSpell)]
+    public class LinearSpell
+    {
+        public ushort Card { get; set; }
+
+        public ushort CardX { get; set; }
+        public uint Source { get; set; }
+        public byte Unknown { get; set; }
+        public ushort CardC { get; set; }
+        public ushort CardCx { get; set; }
+        public int Charges { get; set; }
+        public byte[] Unknown1 { get; set; }
+        public uint Target { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public byte[] Unknown2 { get; set; }
+
+        public float X2 { get; set; }
+
+        public float Y2 { get; set; }
+
+        public LinearSpell(BinaryReader reader, DecoderStore store)
+        {
+            Card = reader.ReadUInt16();
+            CardX = reader.ReadUInt16();
+            Source = reader.ReadUInt32();
+            Unknown = reader.ReadByte();
+            CardC = reader.ReadUInt16();
+            CardCx = reader.ReadUInt16();
+            Charges = reader.ReadInt32();
+            Unknown1 = reader.ReadBytes(18);
+            X = reader.ReadSingle();
+            Y = reader.ReadSingle();
+            Unknown2 = reader.ReadBytes(12);
+            X2 = reader.ReadSingle();
+            Y2 = reader.ReadSingle();
+        }
+    }
+}
---
+++ SkylordsRebornAPI.Replay/ReplayKeys/MoveUnit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace SkylordsRebornAPI.Replay.ReplayKeys
+{
+    [KeyDecoder(Data.ReplayKeys.Move)]
+    public class MoveUnit
+    {
+        public MoveUnit(BinaryReader reader, DecoderStore store)
+        {
+            Target = reader.ReadUInt32();
+            var count = reader.ReadUInt16();
+            Units = new List<uint>();
+            for (int i = 0; i < count; i++)
+            {
+                Units.Add(reader.ReadUInt32());
+            }
+
+            Positions = new List<PointF>();
+            for (int i = 0; i < reader.ReadUInt16()) {
+                Positions.Add(new PointF(reader.ReadSingle(), reader.ReadSingle()));
+            }
+
+            Unknown = reader.ReadBytes(6);
+        }
+
+        public List<PointF> Positions { get; set; }
+
+        public byte[] Unknown { get; set; }
+
+        public List<uint> Units { get; set; }
+
+        public uint Target { get; set; }
+    }
+}
