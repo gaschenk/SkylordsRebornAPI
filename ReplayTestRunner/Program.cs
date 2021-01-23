76555d2ff177b2e48a2e1bad93f1b7dbee0f050d
RaiNote <Timothy.Lutz@gmx.de>
2021-01-23 16:40:54 +0100

    update ReplayTestRunner.csproj

--- ReplayTestRunner/Program.cs
+++ ReplayTestRunner/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System.IO;
-using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using SkylordsRebornAPI.Replay;
 
 namespace ReplayTestRunner
 {
     internal class Program
     {
+        private static readonly JsonSerializerSettings Settings = new()
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            Formatting = Formatting.Indented,
+            DateParseHandling = DateParseHandling.None,
+        };
+
         private static void Main(string[] args)
         {
             ReplayReader reader = new();
             var x = reader.ReadReplay(
-                @"C:\Users\Rai\Downloads\skylords_replay_analyzer\testreplays\autosave.pmv");
-            File.WriteAllText(@".\output.txt",JsonSerializer.Serialize(x));
+                @"C:\Users\Rai\Downloads\skylords_replay_analyzer\testreplays\rpve_22012021_fire.pmv");
+            File.WriteAllText(@".\output.txt", JsonConvert.SerializeObject(x, Settings));
+            return;
         }
     }
 }
